3af73aa01e8f3136f490fa68941d1c78a740e5c8
9347630879 <145848207+9347630879@users.noreply.github.com>
2023-11-02 15:21:00 +0530

    Add files via upload

---
+++ single_level_inheritance.py
@@ -0,0 +1,41 @@
+# single level inheritances
+class Address:
+    __address:str = ""
+    def sumAddress(self,address):
+        self.__address = address
+    def getAddress(self):
+        return self.__address
+class Employee(Address):
+    __firstName:str =""
+    __lastName:str = ""
+    __surname:str = ""
+    def setname(self,fname:str,lname:str,sname:str):
+        self.__firstName = fname
+        self.__lastName = lname
+        self.__surname = sname
+    def __nameformat(self):
+        return f'{self.__firstName} {self.__surname} {self.__lastName} '
+    def getfullName(self):
+        return self.__nameformat()
+emp = Employee()
+emp.setname(fname="Sai",sname="G",lname="Sundhar")
+emp.sumAddress("kurnool")
+print(emp.getfullName())
+print(emp.getAddress())
+
+
+# abstraction with functions
+
+class Employee:
+    __firstName:str = "sai"
+    __lastName:str = "sundhar"
+    def fullname(self):
+        return self.__nameformat(self.__firstName,self.__lastName)
+    def __nameformat(self,fname:str,lname:str):
+        return f'{fname,lname}'
+emp = Employee()
+emp.__firstName = "ABC"
+print(emp.fullname())
+print(emp.__nameformat("sai","sundhar"))
+
+
